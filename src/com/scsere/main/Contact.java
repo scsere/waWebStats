1180d1182738d10f0b5f3a74606f77b0bdd0d4d6
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-10 21:35:57 +0100

    Added class for contacts

---
+++ src/com/scsere/main/Contact.java
@@ -0,0 +1,44 @@
+package com.scsere.main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by scsere on 10/03/16.
+ * Project: waWebStats
+ */
+public class Contact {
+    private WebElement webElement;
+
+    public Contact(WebElement webElement) {
+        this.webElement = webElement;
+    }
+
+    public String getContactName() {
+        if (webElement != null)
+            return webElement.findElement(By.cssSelector(".chat-title")).getText();
+        return null;
+    }
+
+    public String getContactMeta() {
+        if (webElement != null)
+            return webElement.findElement(By.cssSelector(".chat-meta")).getText();
+        return null;
+    }
+
+    public WebElement getWebElement() {
+        return webElement;
+    }
+
+    public void setWebElement(WebElement webElement) {
+        this.webElement = webElement;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name=" + getContactName() +
+                ", meta=" + getContactMeta() +
+                '}';
+    }
+}
