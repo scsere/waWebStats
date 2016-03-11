1fc7db4defd8ddeead3ea2c6ae1528dd718a6f60
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-11 10:54:34 +0100

    Completed ChatListener interface

--- src/com/scsere/main/listeners/ChatListener.java
+++ src/com/scsere/main/listeners/ChatListener.java
@@ -1,14 +1,16 @@
 package com.scsere.main.listeners;
 
+import com.scsere.main.Message;
+
 /**
  * Created by scsere on 11/03/16.
  * Project: waWebStats
  */
 public interface ChatListener {
 
-    void onStatusChanged();
+    void onStatusChanged(String newStatus);
 
-    void onNewMessage();
+    void onNewMessage(Message message);
 
-    void onNewIncomingMessage();
+    void onNewIncomingMessage(Message incomingMessage);
 }
