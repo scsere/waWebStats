0623bd8bc1551218bf08242709f1c4e03f5aa92a
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-11 10:07:53 +0100

    Implemented method to get a list of all messages

--- src/com/scsere/main/ChatFrame.java
+++ src/com/scsere/main/ChatFrame.java
@@ -3,6 +3,7 @@ package com.scsere.main;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,21 +18,24 @@ public class ChatFrame {
         this.frame = frame;
     }
 
-    public String getLastSeenTime(){
+    public String getLastSeenTime() {
         return frame.findElement(By.cssSelector("div.chat-status:nth-child(2)")).getText();
     }
 
-    public void startOnlineListener(){
+    public void startOnlineListener() {
         onlineListener = new OnlineListener(frame.findElement(By.cssSelector("div.chat-status:nth-child(2)")));
     }
 
-    public void stopOnlineListener(){
+    public void stopOnlineListener() {
         onlineListener.setActive(false);
     }
 
-    public List<Message> getMessages(){
-        //TODO: Implement
-        return null;
+    public List<Message> getMessages() {
+        List<Message> messageList = new ArrayList<>();
+        WebElement messageListElement = frame.findElement(By.cssSelector(".message-list"));
+        for (WebElement singleMessage : messageListElement.findElements(By.className("msg")))
+            messageList.add(Message.getMessageFromMsgElement(singleMessage));
+        return messageList;
     }
 
     public WebElement getFrame() {
