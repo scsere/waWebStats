e7e8f076a432063d938b1bace41236b71da5d088
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-14 05:58:34 +0100

    Added another example

---
+++ src/com/scsere/examples/ConversationPrinter.java
@@ -0,0 +1,35 @@
+package com.scsere.examples;
+
+import com.scsere.main.WaWebStats;
+import com.scsere.main.chat.ChatFrame;
+import com.scsere.main.chat.Message;
+import com.scsere.main.contacts.Contact;
+
+import java.util.List;
+
+/**
+ * Created by scsere on 14/03/16.
+ * Project: waWebStats
+ */
+public class ConversationPrinter {
+    public static void main(String[] args) {
+        WaWebStats webStats = new WaWebStats();
+
+        webStats.initFirefox();
+
+        //Fetch all available contacts
+        List<Contact> contacts = webStats.getWebAppFrame().getContacts();
+        //Print all contacts and read user choice
+        Utils.printContacts(contacts);
+        final int selectedContactIndex = Utils.selectContact();
+
+        System.out.println("Selected: " + contacts.get(selectedContactIndex).getContactName());
+
+        ChatFrame chatFrame = webStats.getWebAppFrame().getChatFrameForContact(contacts.get(selectedContactIndex));
+
+        final List<Message> messageList = chatFrame.getMessages();
+
+        for (Message message : messageList)
+            System.out.println(message.getType().toString()+"\tMsg:\t"+message.getText());
+    }
+}
