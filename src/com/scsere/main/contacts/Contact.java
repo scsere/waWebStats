83bbde289eff177c98277407ea7344a90add4696
Viktor Gobbi <viktor.gobbi@gmail.com>
2016-03-16 03:13:28 +0100

    Contacts are now comparable + getter for index
    	->Contacts now implement comparable so a list of contacts can be sorted, they are compared using their index
    	->added a getter for index to contact class

--- src/com/scsere/main/contacts/Contact.java
+++ src/com/scsere/main/contacts/Contact.java
@@ -3,15 +3,27 @@ package com.scsere.main.contacts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by scsere on 10/03/16.
  * Project: waWebStats
  */
-public class Contact {
+public class Contact implements Comparable<Contact> {
     private WebElement webElement;
 
     public Contact(WebElement webElement) {
         this.webElement = webElement;
+    }
+
+    public int getChatIndex() {
+        final String style = webElement.getAttribute("style");
+        final Pattern pattern = Pattern.compile("(\\d+)%");
+        final Matcher matcher = pattern.matcher(style);
+        if (matcher.find())
+            return Integer.parseInt(matcher.group(1)) / 100;
+        return -1;
     }
 
     public String getContactName() {
@@ -32,6 +44,15 @@ public class Contact {
 
     public void setWebElement(WebElement webElement) {
         this.webElement = webElement;
+    }
+
+    @Override
+    public int compareTo(Contact contact) {
+        if (this.getChatIndex() > contact.getChatIndex())
+            return 1;
+        if (this.getChatIndex() < contact.getChatIndex())
+            return -1;
+        return 0;
     }
 
     @Override
